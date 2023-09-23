5217fea15a00fd61c3d19c4fcb3598289dbfee31
atareversei <faraji.ata92@gmail.com>
2023-09-23 17:34:59 +0330

    docs(icon): add usage description

--- src/icon/Icon.tsx
+++ src/icon/Icon.tsx
@@ -1,6 +1,15 @@
 import s from './icon.module.scss'
 import {IconMap} from './icon-map.tsx'
 
+// This project uses icons from Material Design.
+//
+// We are not using a cdn or local icon font because we won't be needing more than
+// 200 icons and downloading all those icons in a font won't do the end user and good.
+//
+// We get the individual icons we need from https://icones.js.org/collection/mdi and
+// extract the svg path and create a component out of them like the ones in ./svg
+// folder.
+
 export type IconTypes = keyof typeof IconMap
 type IconSizes = 'small' | 'medium' | 'large'
 type IconColors = 'text'

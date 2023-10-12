f6758819f0523d13f8b51e176b9a475f36ce3710
atareversei (pc) <faraji.ata92@gmail.com>
2023-10-12 19:42:42 +0330

    fix(button): padding

--- src/app/(landing)/component/navbar/Navbar.tsx
+++ src/app/(landing)/component/navbar/Navbar.tsx
@@ -1,15 +1,23 @@
 import s from './navbar.module.scss'
 import logoSVG from '../../../../../public/luna-logo-v2.svg'
 import Image from 'next/image'
+import Link from 'next/link'
 import {Button} from '@/component/button/Button'
 
 export const Navbar = () => {
   return (
     <nav className={s.container}>
-      <figure className={s.logo}>
-        <Image width={50} height={50} src={logoSVG as string} alt='Luna Logo' />
-        <p>Luna</p>
-      </figure>
+      <Link href='/'>
+        <figure className={s.logo}>
+          <Image
+            width={50}
+            height={50}
+            src={logoSVG as string}
+            alt='Luna Logo'
+          />
+          <p>Luna</p>
+        </figure>
+      </Link>
       <div className={s.mainNav}>
         <Button text='Home' type='tertiary' />
         <Button text='Features' type='tertiary' />
